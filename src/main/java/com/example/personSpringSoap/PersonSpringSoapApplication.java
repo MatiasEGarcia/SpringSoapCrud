092e280c04fe86d02d9182d935abe6021bc37ba0
Matias.E.Garcia <matiasegarcia22@gmail.com>
2022-11-03 20:07:25 -0300

    first commit

---
+++ src/main/java/com/example/personSpringSoap/PersonSpringSoapApplication.java
@@ -0,0 +1,13 @@
+package com.example.personSpringSoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonSpringSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonSpringSoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/personSpringSoap/PersonSpringSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personSpringSoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonSpringSoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
