07262973aee95a2444246d2e73b95efc363a4d73
Matias.E.Garcia <matiasegarcia22@gmail.com>
2022-11-03 20:16:20 -0300

    person entity and java15

---
+++ src/main/java/com/example/personSpringSoap/entity/Person.java
@@ -0,0 +1,38 @@
+package com.example.personSpringSoap.entity;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor @AllArgsConstructor
+@Data
+@Table(name="people" ,schema="soap_test" )
+public class Person {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_person")
+	private Long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "lastname")
+	private String lastname;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "date_of_birth")
+	private Date dateOfBirth;
+}
